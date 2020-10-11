950f61b9dfc4b3ec59407e88621d8e8370d7df9b
Aman Raj <archanaamanraj@gmail.com>
2020-10-12 00:21:10 +0530

    serviceworker-fix

--- next.config.js
+++ next.config.js
@@ -2,26 +2,23 @@ const withOffline = require('next-offline')
 
 module.exports = withOffline({
   workboxOpts: {
-    swDest: 'static/service-worker.js',
     runtimeCaching: [
       {
-        urlPattern: /^https?.*/,
+        urlPattern: /.jpg$/,
+        handler: 'CacheFirst'
+      },
+      {
+        urlPattern: /api/,
         handler: 'NetworkFirst',
         options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: 'static/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
+          cacheableResponse: {
+            statuses: [0, 200],
+            headers: {
+              'x-test': 'true'
+            }
+          }
+        }
+      }
     ]
-  },
+  }
 })
