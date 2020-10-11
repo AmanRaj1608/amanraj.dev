0de52b840351edb50b1ab2a9e892ddf6e6b940f0
Aman Raj <archanaamanraj@gmail.com>
2020-10-12 00:06:10 +0530

    add pwa support

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withOffline = require('next-offline')
 
 module.exports = withOffline({
   workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
+    swDest: 'static/service-worker.js',
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
