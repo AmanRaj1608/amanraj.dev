801323df990dbdd0449db449b07f3ae25a9381d1
Aman Raj <42104907+AmanRaj1608@users.noreply.github.com>
2022-01-22 16:02:18 +0530

    Trying clarity

--- pages/_document.js
+++ pages/_document.js
@@ -58,6 +58,17 @@ class MyDocument extends Document {
             }}
           />
 
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "aaotps71tq");
+          `,
+            }}
+          />
           <link rel="prefetch" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&family=PT+Mono&display=swap" as="font" type="font" crossOrigin="true" />
         </Head>
         <body>
